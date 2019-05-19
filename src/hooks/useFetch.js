9be6d023d0b582f4476b0637629f0ff2f00b1355
Shane-Lewes Tenorio <sltenorio99@gmail.com>
2019-05-19 15:11:55 -0500

    💅 Convert useFetch() custom hook to use async/await

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,10 +4,14 @@ import { useState, useEffect } from "react";
 const useFetch = url => {
   const [data, setData] = useState([]);
 
+  const getData = async () => {
+    const response = await fetch(url); // fetch from passed in url
+    const data = await response.json();
+    setData(data); // call setData() to set data state
+  };
+
   useEffect(() => {
-    fetch(url) // fetch from passed in url
-      .then(response => response.json())
-      .then(data => setData(data)); // call setData() to set data state
+    getData();
   }, []); /* <- only re-run when value inside [] changes (in this case: never)
     https://reactjs.org/docs/hooks-effect.html#tip-optimizing-performance-by-skipping-effects
     */
